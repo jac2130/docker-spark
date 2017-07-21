f282eaf9d231a2280f03e393040a06e0c330e22c
Johannes Castner <jcastner@ebay.com>
2017-07-21 11:17:29 -0600

    save repo

--- files/main.sh
+++
@@ -1,6 +0,0 @@
-if [[ -z $DOCKER_SPARK_BRANCH ]]; then
-    export DOCKER_SPARK_BRANCH="master"
-fi
-git clone -b "$DOCKER_SPARK_BRANCH" docker-spark:jac2130/docker-spark.git
-
-apache2ctl -D FOREGROUND
